101d507522d95ae11399c639b7a82df951eee913
DERACO Stéphane <stephane.deraco@dsi.cnrs.fr>
2015-06-29 17:52:18 +0200

    Récupération des propriétés avec @Value

--- src/main/java/org/jdev2015/services/Process.java
+++ src/main/java/org/jdev2015/services/Process.java
@@ -1,6 +1,7 @@
 package org.jdev2015.services;
 
 import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -10,8 +11,11 @@ import static org.slf4j.LoggerFactory.getLogger;
 public class Process implements CommandLineRunner {
 	private static final Logger LOG = getLogger(Process.class);
 
+	@Value("${files.in}")
+	private String inputDir;
+
 	@Override
 	public void run(String... args) throws Exception {
-		LOG.debug("C'est parti !!!");
+		LOG.debug("Fetching files from {}", inputDir);
 	}
 }
